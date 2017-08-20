81c333ced58862167283d153e3dec6656dafe0c6
Backs <rogatnev.sergey@gmail.com>
2017-08-20 19:27:21 +0700

    - Levenshtein algorithm

---
+++ WikiImages.Algorithm/Levenshtein.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace WikiImages.Algorithm
+{
+    public static class Levenshtein
+    {
+        public static int Distance(string string1, string string2)
+        {
+            if (string1 == null)
+                throw new ArgumentNullException(nameof(string1));
+            if (string2 == null)
+                throw new ArgumentNullException(nameof(string2));
+
+            string1 = string1.ToLowerInvariant();
+            string2 = string2.ToLowerInvariant();
+
+            var m = new int[string1.Length + 1, string2.Length + 1];
+
+            for (var i = 0; i <= string1.Length; i++)
+            {
+                m[i, 0] = i;
+            }
+            for (var j = 0; j <= string2.Length; j++)
+            {
+                m[0, j] = j;
+            }
+
+            for (var i = 1; i <= string1.Length; i++)
+            {
+                for (var j = 1; j <= string2.Length; j++)
+                {
+                    var diff = string1[i - 1] == string2[j - 1] ? 0 : 1;
+
+                    m[i, j] = Math.Min(
+                        Math.Min(m[i - 1, j] + 1,
+                            m[i, j - 1] + 1),
+                        m[i - 1, j - 1] + diff);
+                }
+            }
+            return m[string1.Length, string2.Length];
+        }
+
+        public static bool AreSame(string string1, string string2, int maxDifference)
+        {
+            if (maxDifference < 0)
+                throw new ArgumentOutOfRangeException(nameof(maxDifference));
+
+            return Distance(string1, string2) <= maxDifference;
+        }
+    }
+}
---
+++ WikiImages.Tests/Algorithm/LevenshteinTests/LevenshteinDistanceTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using WikiImages.Algorithm;
+
+namespace WikiImages.Tests.Algorithm.LevenshteinTests
+{
+    [TestFixture]
+    public class LevenshteinDistanceTests
+    {
+        [Test]
+        public void EqualsTest()
+        {
+            var result = Levenshtein.Distance("abs", "abs");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void IgnoreCaseTest()
+        {
+            var result = Levenshtein.Distance("abs", "ABS");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReplaceTest()
+        {
+            var result = Levenshtein.Distance("abs", "abc");
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            var result = Levenshtein.Distance("abs", "ab");
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SwapTest()
+        {
+            var result = Levenshtein.Distance("ab", "ba");
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ComplexTest()
+        {
+            var result = Levenshtein.Distance("ca", "abc");
+
+            //ca -> ac -> abc
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
